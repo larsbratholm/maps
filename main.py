2bda79015cb55b6d945031c4186962b6fda26d98
Lars Andersen Bratholm <larsbratholm@gmail.com>
2019-07-06 17:27:24 +0100

    Fiddling with mixture

--- main.py
+++ main.py
@@ -135,10 +135,16 @@ def load_data(path):
 
 def missing_001(data):
     """
-    Removes all entries where either comp_wend, comp_week or has_dep_diag is missing.
+    Adds a new column named 'n_ignore' that indicates where either comp_wend, comp_week or has_dep_diag are missing.
     """
+    import numpy as np
     mask = (data.comp_wend.isnull() | data.comp_week.isnull() | data.has_dep_diag.isnull())
-    return data[~mask]
+    n = len(data)
+    values = np.zeros(n, dtype=bool)
+    values[mask] = True
+    data['n_ignore'] = values
+
+    return data
 
 def depression_001(data):
     """
@@ -148,7 +154,7 @@ def depression_001(data):
     import numpy as np
 
     # Make masks to indicate depression
-    mask = data.has_dep_diag.values == "Yes ICD-10 diagnosis of depression"
+    mask = (data.has_dep_diag == "Yes ICD-10 diagnosis of depression")
     # Construct depression column
     n = len(data)
     values = np.zeros(n, dtype=bool)
@@ -165,14 +171,14 @@ def computer_use_001(data):
     import numpy as np
 
     # Make masks to indicate computer use
-    mask_comp_wend_0 = (data.comp_wend.values == "Not at all")
-    mask_comp_wend_1 = (data.comp_wend.values == "Less than 1 hour")
-    mask_comp_wend_2 = (data.comp_wend.values == "1-2 hours")
-    mask_comp_wend_3 = (data.comp_wend.values == "3 or more hours")
-    mask_comp_week_0 = (data.comp_week.values == "Not at all")
-    mask_comp_week_1 = (data.comp_week.values == "Less than 1 hour")
-    mask_comp_week_2 = (data.comp_week.values == "1-2 hours")
-    mask_comp_week_3 = (data.comp_week.values == "3 or more hours")
+    mask_comp_wend_0 = (data.comp_wend == "Not at all")
+    mask_comp_wend_1 = (data.comp_wend == "Less than 1 hour")
+    mask_comp_wend_2 = (data.comp_wend == "1-2 hours")
+    mask_comp_wend_3 = (data.comp_wend == "3 or more hours")
+    mask_comp_week_0 = (data.comp_week == "Not at all")
+    mask_comp_week_1 = (data.comp_week == "Less than 1 hour")
+    mask_comp_week_2 = (data.comp_week == "1-2 hours")
+    mask_comp_week_3 = (data.comp_week == "3 or more hours")
 
     # Low/High computer use according to some arbitrary definition
     mask_comp_low = (mask_comp_wend_0 | mask_comp_wend_1) & (mask_comp_week_0 | mask_comp_wend_1)
@@ -191,31 +197,70 @@ def computer_use_001(data):
 
 def missing_002(data):
     """
-    Further remove all entries where computer use is neither high or low
+    Update ignore column to include all entries where computer use is neither high or low
     """
-    mask = data['comp_use'] == "other"
-    return data[~mask]
+    mask = (data.comp_use == "other")
+    values = data.n_ignore.values
+    values[mask] = True
+    data['n_ignore'] = values
+    return data
+
+# TODO cluster computer use
+# TODO get conditional distribution
+# TODO figure out missing
+# TODO add computer use to model
 
 def specify_model(data):
     """
     Create simple MLE model
     """
     import numpy as np
-    import scipy.stats as ss
+    import bayespy
 
     # Create some masks of the data
     mask_depression = (data.depression == True)
-    mask_high_computer_use = (data['comp_use'] == 'high')
+    mask_high_computer_use = (data.comp_use == 'high')
+    depression_observables = mask_depression.values[:,None]
+    computer_use_observables = mask_high_computer_use.values[:,None]
 
-    # get counts needed for MLE estimates
-    count_comp_low_dep_no = sum(~mask_depression & ~mask_high_computer_use)
-    count_comp_low_dep_yes = sum(mask_depression & ~mask_high_computer_use)
-    count_comp_high_dep_no = sum(~mask_depression & mask_high_computer_use)
-    count_comp_high_dep_yes = sum(mask_depression & mask_high_computer_use)
+    # Keep only data points that we chose not to ignore
+    mask = (~data.n_ignore).values
 
-    # Get the probabilities of having depression and high computer use (p_YH),
-    # depression and low computer use (p_YL), no depression and high computer use
-    # (p_NH) and no depression and low computer use (p_NL)
+
+    # Hidden nodes
+    n_hidden_nodes = 1
+    # Data size
+    n_samples = data.shape[0]
+    # Probability for specific hidden nodes (plates = (n_hidden_nodes,)
+    # This correspond to the weight prefactor of the independent mixture model
+    # 1/2 is Jeffreys prior
+    p_hidden_node = bayespy.nodes.Dirichlet([1/2 for n in range(n_hidden_nodes)])
+    # Observable for which mixture a sample belong to. Hidden. (plates = (n_samples, 1))
+    obs_hidden_node = bayespy.nodes.Categorical(p_hidden_node, plates=(n_samples, 1))
+    # Probability of depression in each node (plates = (1, n_hidden_nodes)
+    p_depression = bayespy.nodes.Beta([1/2,1/2], plates=(1,n_hidden_nodes))
+    obs_depression = bayespy.nodes.Mixture(obs_hidden_node, bayespy.nodes.Bernoulli, p_depression)
+    # Probability of high computer use in each node (plates = (1, n_hidden_nodes)
+    p_computer_use = bayespy.nodes.Beta([1/2,1/2], plates=(1,n_hidden_nodes))
+    obs_computer_use = bayespy.nodes.Mixture(obs_hidden_node, bayespy.nodes.Bernoulli, p_computer_use)
+    # Construct model
+    model = bayespy.inference.VB(obs_hidden_node, p_hidden_node, obs_depression, p_depression, obs_computer_use, p_computer_use)
+    # Break symmetry
+    p_depression.initialize_from_random()
+    p_computer_use.initialize_from_random()
+    # Observe data
+    obs_depression.observe(depression_observables[:,None], mask=mask[:,None])
+    obs_computer_use.observe(computer_use_observables[:,None], mask=mask[:,None])
+    model.update(repeat=1000, verbose=True, tol=1e-6)
+    print(p_depression)
+    print(p_computer_use)
+    print(p_hidden_node)
+    quit()
+
+
+    # Get the probabilities of having depression given high computer use (p_YH),
+    # depression given low computer use (p_YL), no depression given high computer use
+    # (p_NH) and no depression given low computer use (p_NL)
     p_YH = count_comp_high_dep_yes / (count_comp_high_dep_no + count_comp_high_dep_yes)
     p_NH = count_comp_high_dep_no / (count_comp_high_dep_no + count_comp_high_dep_yes)
     p_YL = count_comp_low_dep_yes / (count_comp_low_dep_no + count_comp_low_dep_yes)
@@ -229,14 +274,23 @@ def specify_model(data):
     L = np.log(p_YH) - np.log(p_NH) - np.log(p_YL) + np.log(p_NL)
     # Variance
     se = np.sqrt(1/count_comp_low_dep_no + 1/count_comp_low_dep_yes + 1/count_comp_high_dep_no + 1/count_comp_high_dep_yes)
-    # Odds-ratio
+    # Odds-ratio (odds of depression given high computer use over odds of depression given low computer use)
     OR = np.exp(L)
     # Confidence interval
     ci = (np.exp(L-1.96*se), np.exp(L+1.96*se))
     # 2-sided p-value
     p_value = ss.norm.sf(abs(L/se))*2
-    p_value2 = (1-ss.norm.cdf(abs(L/se)))*2
-    print(p_value, p_value2)
+
+    # Just calculate the maximum likelihood for the remaining data, with a model
+    # placing each point in 4 categories as above. I don't really have any hidden variables
+    # so I don't think an information criterion makes sense on it's own
+    log_likelihood = count_comp_low_dep_no * np.log(p_NL) \
+                   + count_comp_low_dep_yes * np.log(p_YL) \
+                   + count_comp_high_dep_no * np.log(p_NH) \
+                   + count_comp_high_dep_yes * np.log(p_YH)
+
+    # 4 categories, so 3 variables
+    aic = 2 * 3 - 2 * log_likelihood
 
     # Make dictionary for results
     results = {}
@@ -252,8 +306,8 @@ def specify_model(data):
     #results['or_2'] = None
     #results['ci_2'] = None
     #results['p_2'] = None
-
-
+    # AIC
+    results['AIC'] = aic
 
     return data, results
 
@@ -266,3 +320,4 @@ if __name__ == "__main__":
     data = computer_use_001(data)
     data = missing_002(data)
     data, results = specify_model(data)
+    print(results)
